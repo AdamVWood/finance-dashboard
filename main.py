92af5dd19b7b4b1d2029e580e4abd46c20398e43
Adam Wood <79604223+Add9treetrunks@users.noreply.github.com>
2026-08-20 15:18:33 -0400

    Started HTML code in index.html and converted main.py to work with flask

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+from flask import Flask, render_template
 from database.database import setup
-from app.dashboard import main_menu
 
+app = Flask(__name__)
 setup()
-main_menu()
+
+@app.route("/")
+def dashboard():
+    return render_template("index.html")
+
+@app.route("/transactions")
+def transactions_page():
+    return render_template("transactions.html")
+
+
+@app.route("/budgets")
+def budgets_page():
+    return render_template("budgets.html")
+
+@app.route("/savings")
+def savings_page():
+    return render_template("savings.html")
+
+@app.route("/investments")
+def investments_page():
+    return render_template("investments.html")
+
+@app.route("/categories")
+def categories_page():
+    return render_template("categories.html")
+
+@app.route("/reports")
+def reports_page():
+    return render_template("reports.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
